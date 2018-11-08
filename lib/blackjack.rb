618fa77aa6e24ba8cc2780aeb2b60b52bd292455
Albina Zakirova <zak1804@gmail.com>
2018-11-08 20:07:20 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,8 +31,9 @@ end
 def hit?(card_total)
   prompt_user
   input = get_user_input
-  if input == 'h' 
-    deal_card
+  if input == 'h'
+    card_total+= deal_card
+ deal_card
   elsif input == 's'
     return card_total
   else 
